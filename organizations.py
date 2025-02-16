ee30999c6a3207be2cab3ecf15aa01c9a261ad95
Will Sarg <12886992+willsarg@users.noreply.github.com>
2025-02-16 12:37:21 -0500

    gitignore obsidian

---
+++ organizations.py
@@ -0,0 +1,105 @@
+#Dictionary of Organizations, Corporations, Departments, etc.
+#Potentially involved in the MA Energy Infrastructure
+
+orgList = {
+    "ISO-NE": {
+        "name": "ISO New England",
+        "description": "The Independent System Operator of New England, responsible for the operation of the region's bulk electric power system and transmission lines.",
+        "website": "http://www.iso-ne.com/"
+    },
+    "FERC": {
+        "name": "Federal Energy Regulatory Commission",
+        "description": "The Federal Energy Regulatory Commission, an independent agency that regulates the interstate transmission of electricity, natural gas, and oil.",
+        "website": "https://www.ferc.gov/"
+    },
+    "NEPOOL": {
+        "name": "New England Power Pool",
+        "description": "The New England Power Pool, a voluntary organization of market participants in the New England electricity market.",
+        "website": "https://www.nepool.com/"
+    },
+    "Eversource": {
+        "name": "Eversource Energy",
+        "description": "Eversource Energy, a public utility holding company that operates electric, natural gas, and water utilities in New England.",
+        "website": "https://www.eversource.com/"
+    },
+    "National Grid": {
+        "name": "National Grid",
+        "description": "National Grid, a British multinational electricity and gas utility company that operates in the northeastern United States.",
+        "website": "https://www.nationalgridus.com/"
+    },
+    "Enbridge": {
+        "name": "Enbridge Inc.",
+        "description": "Enbridge Inc., a Canadian multinational energy transportation company that operates the Algonquin Gas Transmission pipeline.",
+        "website": "https://www.enbridge.com/"
+    },
+    "Kinder Morgan": {
+        "name": "Kinder Morgan",
+        "description": "Kinder Morgan, an American energy infrastructure company that operates the Tennessee Gas Pipeline.",
+        "website": "https://www.kindermorgan.com/"
+    },
+    "Spectra Energy": {
+        "name": "Spectra Energy",
+        "description": "Spectra Energy, an American energy infrastructure company that operates the Algonquin Gas Transmission pipeline.",
+        "website": "https://www.spectraenergy.com/"
+    },
+    "Algonquin Gas Transmission": {
+        "name": "Algonquin Gas Transmission",
+        "description": "Algonquin Gas Transmission, a natural gas pipeline system that delivers gas from New Jersey to Massachusetts.",
+        "website": "https://www.spectraenergy.com/Operations/US-Natural-Gas-Operations/Algonquin-Gas-Transmission/"
+    },
+    "Tennessee Gas Pipeline": {
+        "name": "Tennessee Gas Pipeline",
+        "description": "Tennessee Gas Pipeline, a natural gas pipeline system that delivers gas from the Gulf of Mexico to New England.",
+        "website": "https://www.kindermorgan.com/business/gas_pipelines/east/tnp.cfm"
+    },
+    "Maritimes & Northeast Pipeline": {
+        "name": "Maritimes & Northeast Pipeline",
+        "description": "Maritimes & Northeast Pipeline, a natural gas pipeline system that delivers gas from Nova Scotia to Massachusetts.",
+        "website": "https://www.mnpp.com/"
+    },
+    "Columbia Gas": {
+        "name": "Columbia Gas",
+        "description": "Columbia Gas, a natural gas utility company that operates in Massachusetts.",
+        "website": "https://www.columbiagasma.com/"
+    },
+    "Berkshire Gas": {
+        "name": "Berkshire Gas",
+        "description": "Berkshire Gas, a natural gas utility company that operates in western Massachusetts.",
+        "website": "https://www.berkshiregas.com/"
+    },
+    "Liberty Utilities": {
+        "name": "Liberty Utilities",
+        "description": "Liberty Utilities, a natural gas utility company that operates in New Hampshire.",
+        "website": "https://www.libertyutilities.com/"
+    },
+    "Unitil": {
+        "name": "Unitil",
+        "description": "Unitil, a natural gas utility company that operates in Massachusetts, New Hampshire, and Maine.",
+        "website": "https://www.unitil.com/"
+    },
+    "Massachusetts Department of Public Utilities": {
+        "name": "Massachusetts Department of Public Utilities",
+        "description": "The Massachusetts Department of Public Utilities, a state agency that regulates the energy and telecommunications industries.",
+        "website": "https://www.mass.gov/orgs/department-of-public-utilities"
+    },
+    "Massachusetts Department of Energy Resources": {
+        "name": "Massachusetts Department of Energy Resources",
+        "description": "The Massachusetts Department of Energy Resources, a state agency that promotes energy efficiency and renewable energy.",
+        "website": "https://www.mass.gov/orgs/department-of-energy-resources"
+    },
+    "Massachusetts Department of Environmental Protection": {
+        "name": "Massachusetts Department of Environmental Protection",
+        "description": "The Massachusetts Department of Environmental Protection, a state agency that protects the environment and public health.",
+        "website": "https://www.mass.gov/orgs/massachusetts-department-of-environmental-protection"
+    },
+    "Massachusetts Attorney General": {
+        "name": "Massachusetts Attorney General",
+        "description": "The Massachusetts Attorney General, the chief legal officer of the Commonwealth of Massachusetts.",
+        "website": "https://www.mass.gov/orgs/office-of-attorney-general-maura-healey"
+    },
+    "Massachusetts Executive Office of Energy and Environmental Affairs": {
+        "name": "Massachusetts Executive Office of Energy and Environmental Affairs",
+        "description": "The Massachusetts Executive Office of Energy and Environmental Affairs, a state agency that oversees energy and environmental policy.",
+        "website": "https://www.mass.gov/orgs/executive-office-of-energy-and-environmental-affairs"
+    }
+}
